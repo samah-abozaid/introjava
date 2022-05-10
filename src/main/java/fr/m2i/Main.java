43f006825feec57d117ffd50098a216f2d6c94be
samah-abozaid <samah.abozaid89@gmail.com>
2022-05-10 16:29:29 +0200

    premier commit

---
+++ src/main/java/fr/m2i/Main.java
@@ -0,0 +1,20 @@
+package fr.m2i;
+public class Main {
+    public static void main(String[] args) {
+
+
+        int x = 8;
+        int y = 9;
+        int z = x + y;
+        System.out.println(z);
+    }
+}
+
+
+
+
+
+
+
+
+
